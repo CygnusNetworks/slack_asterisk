0b668017d2a9add74633e3099a67415f85a5fca3
cygnusb <cygnusb@users.noreply.github.com>
2025-11-17 13:22:04 +0100

    Replace `asterisk_agi` dependency with minimal built-in AGI helpers to simplify AGI environment handling and variable management.

--- slack_asterisk/agi_server.py
+++ slack_asterisk/agi_server.py
@@ -7,8 +7,6 @@ import sys
 
 from slack_sdk import WebClient
 
-from . import asterisk_agi
-
 log = logging.getLogger("slack_asterisk")
 LOG_SPEC = "%(name)s[%(process)s]: %(filename)s:%(lineno)d/%(funcName)s###%(message)s"
 
@@ -29,28 +27,116 @@ _configure_logging_from_env()
 
 
 class SlackAsterisk(socketserver.StreamRequestHandler):
-	@staticmethod
-	def get_vars(agi):
+
+	# --- Minimal built-in AGI helpers (replacing asterisk_agi) ---
+	def _readline(self):
+		"""Read a single line from the socket, return str without CRLF or None."""
+		data = b""
+		try:
+			while not data.endswith(b"\n"):
+				chunk = self.request.recv(1)
+				if not chunk:
+					break
+				data += chunk
+		except Exception as e:  # pragma: no cover
+			log.debug("Error while reading line from socket: %s", e)
+		if not data:
+			return None
+		return data.decode(errors="ignore").rstrip("\r\n")
+
+	def _read_agi_env(self):
+		"""Read AGI environment lines until a blank line, return dict."""
+		env = {}
+		while True:
+			line = self._readline()
+			if line is None:
+				break
+			if line == "":
+				break
+			parts = line.split(":", 1)
+			if len(parts) == 2:
+				env[parts[0].strip()] = parts[1].strip()
+		log.debug("AGI env: %s", env)
+		return env
+
+	def _send_cmd(self, cmd: str) -> str:
+		"""Send a raw AGI command and return the single-line reply."""
+		log.debug("AGI >> %s", cmd)
+		try:
+			self.request.sendall((cmd + "\n").encode())
+		except Exception as e:  # pragma: no cover
+			log.debug("Error sending AGI command: %s", e)
+		reply = self._readline() or ""
+		log.debug("AGI << %s", reply)
+		return reply
+
+	def _set_var(self, name: str, value: str | None) -> None:
+		if value is None:
+			value = ""
+		safe = value.replace('"', '\\"')
+		self._send_cmd(f'SET VARIABLE {name} "{safe}"')
+
+	def get_variable(self, name: str) -> str | None:
+		"""Get an Asterisk variable value via AGI.
+
+		Parses typical responses like:
+		  200 result=1 (foo)
+		  200 result=0
+		Returns the string value or None if not set.
+		"""
+		reply = self._send_cmd(f"GET VARIABLE {name}")
+		# Quick parse
+		# Find result=
+		result_pos = reply.find("result=")
+		if result_pos == -1:
+			return None
+		try:
+			after = reply[result_pos + len("result="):]
+			# result may be like 1 (value) or 0
+			# extract integer result
+			res_str = ""
+			i = 0
+			while i < len(after) and after[i].isdigit():
+				res_str += after[i]
+				i += 1
+			res = int(res_str) if res_str else 0
+			if res == 0:
+				return None
+			# try to extract value inside parentheses
+			lpar = reply.find("(")
+			rpar = reply.rfind(")")
+			if lpar != -1 and rpar != -1 and rpar > lpar:
+				val = reply[lpar + 1:rpar]
+				# Some asterisk versions wrap empty value as ""; normalize
+				if val == '""':
+					return ""
+				return val
+			# sometimes value is quoted without parentheses in some impls
+			return ""
+		except Exception:  # pragma: no cover
+			return None
+
+	def get_vars(self):
 		chan_vars = dict()
-		chan_vars["callerid_num"] = agi.get_variable('CALLERID(num)')
-		chan_vars["callerid_name"] = agi.get_variable('CALLERID(name)')
-		chan_vars["uniqueid"] = agi.get_variable('UNIQUEID')
-		chan_vars["arg1"] = agi.get_variable('ARG1')
-		chan_vars["refid"] = agi.get_variable('SLACK_ASTERISK_REFID')
-		chan_vars["info_text"] = agi.get_variable('SLACK_ASTERISK_INFO_TEXT')
+		chan_vars["callerid_num"] = self.get_variable('CALLERID(num)')
+		chan_vars["callerid_name"] = self.get_variable('CALLERID(name)')
+		chan_vars["uniqueid"] = self.get_variable('UNIQUEID')
+		chan_vars["arg1"] = self.get_variable('ARG1')
+		chan_vars["refid"] = self.get_variable('SLACK_ASTERISK_REFID')
+		chan_vars["info_text"] = self.get_variable('SLACK_ASTERISK_INFO_TEXT')
 
-		chan_vars["dialstatus"] = agi.get_variable('DIALSTATUS')
-		chan_vars["dialedpeername"] = agi.get_variable('DIALEDPEERNAME')
-		chan_vars["dialedpeernumber"] = agi.get_variable('DIALEDPEERNUMBER')
-		chan_vars["dialedtime"] = agi.get_variable("DIALEDTIME")
-		chan_vars["answeredtime"] = agi.get_variable("ANSWEREDTIME")
-		chan_vars["exten"] = agi.get_variable('EXTEN')
-		chan_vars["hangupcause"] = agi.get_variable('HANGUPCAUSE')
+		chan_vars["dialstatus"] = self.get_variable('DIALSTATUS')
+		chan_vars["dialedpeername"] = self.get_variable('DIALEDPEERNAME')
+		chan_vars["dialedpeernumber"] = self.get_variable('DIALEDPEERNUMBER')
+		chan_vars["dialedtime"] = self.get_variable("DIALEDTIME")
+		chan_vars["answeredtime"] = self.get_variable("ANSWEREDTIME")
+		chan_vars["exten"] = self.get_variable('EXTEN')
+		chan_vars["hangupcause"] = self.get_variable('HANGUPCAUSE')
 
-		chan_vars["type"] = agi.get_variable('SLACK_ASTERISK_TYPE')
-		chan_vars["color"] = agi.get_variable('SLACK_ASTERISK_COLOR')
+		chan_vars["type"] = self.get_variable('SLACK_ASTERISK_TYPE')
+		chan_vars["color"] = self.get_variable('SLACK_ASTERISK_COLOR')
 
-		chan_vars["direction"] = agi.get_variable('SLACK_ASTERISK_DIRECTION')
+		chan_vars["direction"] = self.get_variable('SLACK_ASTERISK_DIRECTION')
 
 		to_delete = []
 		for k, v in chan_vars.items():
@@ -129,13 +215,10 @@ class SlackAsterisk(socketserver.StreamRequestHandler):
 	def handle(self):  # pylint:disable=too-many-statements
 		log.debug("Received FastAGI request for client %s:%s", self.client_address[0], self.client_address[1])
 		try:  # pylint:disable=too-many-nested-blocks
-			agi = asterisk_agi.AGI(self.rfile, self.wfile)
-			# Debug log of raw AGI environment variables
-			try:
-				log.debug("AGI env: %s", getattr(agi, 'env', {}))
-			except Exception as _e:  # pragma: no cover
-				log.debug("Unable to log AGI env: %s", _e)
-			channel_vars = self.get_vars(agi)
+			# Read and log AGI environment
+			_env = self._read_agi_env()
+			# Collect channel vars using minimal GET VARIABLE calls
+			channel_vars = self.get_vars()
 			log.debug("FastAGI channel vars from %s:%s -> %s", self.client_address[0], self.client_address[1], channel_vars)
 
 			new_call = False
@@ -160,7 +243,7 @@ class SlackAsterisk(socketserver.StreamRequestHandler):
 						msg_data["from_name"] = "anonymous"
 					if msg_data["to_num"] is None:
 						msg_data["to_num"] = channel_vars["exten"]
-					agi.set_variable("SLACK_ASTERISK_REFID", channel_vars["uniqueid"])
+					self._set_var("SLACK_ASTERISK_REFID", channel_vars["uniqueid"])
 					new_call = True
 				else:
 					msg_data = self.server.calls_dict[channel_vars["uniqueid"]]
@@ -190,11 +273,11 @@ class SlackAsterisk(socketserver.StreamRequestHandler):
 				msg_data["ts"] = ts
 				msg_data["channel"] = channel
 			elif "arg1" in channel_vars:
-				log.debug("Picked up call detected for uniqueid %s", channel_vars["uniqueid"])
+				log.debug("Picked up call detected for uniqueid %s", channel_vars["uniqueid"]) 
 				# this is a picked up call in a dial M macro
 				if "dialedpeernumber" in channel_vars:
 					log.debug("Found dp number in channel vars %s", channel_vars)
-					msg_data["to_num"] = self.get_dialedpeernumber(channel_vars["dialedpeernumber"])
+					msg_data["to_num"] = self.get_dialedpeernumber(channel_vars["dialedpeernumber"]) 
 				else:
 					log.debug("No dialed peer number in channel vars %s", channel_vars)
 					if "callerid_num" in channel_vars:
@@ -204,7 +287,7 @@ class SlackAsterisk(socketserver.StreamRequestHandler):
 				dest = self.get_destination(msg_data)
 				self.update_message("☑️ Call established with %s" % dest, msg_data)
 			elif "dialstatus" in channel_vars:
-				log.debug("finished call detected for uniqueid %s", channel_vars["uniqueid"])
+				log.debug("finished call detected for uniqueid %s", channel_vars["uniqueid"]) 
 				dest = self.get_destination(msg_data)
 				# set color to grey as default
 				color = "#333333"
@@ -249,10 +332,6 @@ class SlackAsterisk(socketserver.StreamRequestHandler):
 				self.update_message("Unknown call state", msg_data)
 
 		except Exception as e:
-			try:
-				del agi
-			except Exception:
-				pass
 			log.exception("Exception occurred with message %s", e)
 
 
